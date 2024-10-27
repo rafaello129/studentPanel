4eb86239c9ec1314832a8c5411a5ca12703227ac
BrendaTellez <BrendaTellez>
2024-10-26 22:46:29 -0500

    Mejorar aspecto visual de la tarjeta de estudiante

--- src/dashboard/components/student/StudentCard.tsx
+++ src/dashboard/components/student/StudentCard.tsx
@@ -9,10 +9,10 @@ interface StudentCardProps {
 
 const StudentCard: React.FC<StudentCardProps> = ({ student, onSelect }) => {
   return (
-    <div className="card student-card m-3 p-4" key={student.noControl}>
+    <div className="card student-card m-3 p-4 col-md-5 mb-4" key={student.noControl}>
       <div className="d-flex">
         <img src={user} width={70} className="m-2 p-2" alt="..." />
-        <h5 className="card-title mt-3">
+        <h5 className="card-title mt-3"> 
           {student.name + " " + student.lastName + " " + student.motherLastName}
         </h5>
       </div>
